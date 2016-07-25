c84aff0e2242a978f37015136059b7f5105552ba
MadMan <nuna.vlad@yahoo.com>
2016-07-25 20:34:46 +0300

    Core/Spells: Fix Hellscream's Warsong & Strength of Wrynn buffs

---
+++ sql/spell fixes/spell_area_icc_buffs.sql
@@ -0,0 +1,6 @@
+-- Hellscream's Warsong
+UPDATE `spell_area` SET `spell`='73822'
+WHERE `spell`='73818' AND `area`='4812' AND `quest_start`='0' AND `aura_spell`='0' AND `racemask`='690' AND `gender`='2';
+-- Strength of Wrynn
+UPDATE `spell_area` SET `spell`='73828'
+WHERE `spell`='73824' AND `area`='4812' AND `quest_start`='0' AND `aura_spell`='0' AND `racemask`='1101' AND `gender`='2';
